51f0f7d15c22bd9ee761a54f7f10edd1ae86ec36
fatinelham29 <fatinelham29@gmail.com>
2025-09-15 18:33:31 +0600

    Add files via upload

---
+++ _1_hello.java
@@ -0,0 +1,12 @@
+
+package w3basicjava150;
+
+public class _1_hello {
+    
+        public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("My name is Fatin Ilham");
+    }
+
+    
+}
